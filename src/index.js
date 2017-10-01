e47e9c38dbd9a9b6df2b6e31d92eb235c035f69d
Rajashekar Chintalapati <rchintalapati@walmartlabs.com>
2017-09-30 17:11:43 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import { createStore } from 'redux'
+import reducer from './reducers'
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+registerServiceWorker();
